d75e66708a582e39d150205a93956790d36c6fc7
Samuel Pereira <gmr.quest.10@gmail.com>
2023-04-12 09:22:11 -0300

    chore: cors setup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import cors, { CorsOptions } from "cors";
+
 import Logger from "../config/logger";
 
 const app = express();
 
 const PORT = process.env.PORT;
+
+const corsOptions: CorsOptions = {
+  allowedHeaders: ["*"],
+  credentials: true,
+  methods: "POST,GET,PUT,PATCH,DELETE,OPTIONS,HEAD",
+  origin: "*",
+  preflightContinue: false,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
