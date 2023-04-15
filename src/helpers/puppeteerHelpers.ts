c1ab2b36c96bd56e564172e20c30d623f319d33b
Samuel Pereira <gmr.quest.10@gmail.com>
2023-04-15 17:18:27 -0300

    fix: remove Puppeteer, now using JSDOM

--- src/helpers/puppeteerHelpers.ts
+++ src/helpers/puppeteerHelpers.ts
@@ -1,4 +1,5 @@
-import puppeteer from "puppeteer";
+import { JSDOM } from "jsdom";
+
 import {
   IProduct,
   ISearchByCategory,
@@ -10,157 +11,124 @@ import {
 } from "../enums/enums";
 
 const scrapingMercadoLivre = async (url: string) => {
-  const browser = await puppeteer.launch({
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-    headless: true,
-    args: ["--no-sandbox"],
-    ignoreHTTPSErrors: true,
-  });
+  const response = await fetch(url);
+  const html = await response.text();
 
-  const page = await browser.newPage();
-  await page.goto(url);
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
 
-  const products = await page.evaluate(() => {
-    const productCards: IProduct[] = [
-      ...document.querySelectorAll(
-        "ol.ui-search-layout ui-search-layout--stack li.ui-search-layout__item div.ui-search-result__wrapper div.ui-search-result"
-      ),
-    ].map((productCard) => {
-      const productInfos = {
-        description: productCard.querySelector(
-          "div.ui-search-result__content-wrapper div.ui-search-item__group--title a.shops__items-group-details h2.ui-search-item__title"
-        ).textContent,
-        imageUrl: productCard
-          .querySelector(
-            "div.ui-search-result__image a div.slick-track div.slick-active img.ui-search-result-image__element"
-          )
-          .getAttribute("src")
-          .toString(),
-        price: productCard.querySelector(
-          "div.ui-search-result__content-wrapper div.ui-search-result__content-columns div div div div div span span.price-tag-amount"
-        ).textContent,
-        website: productCard
-          .querySelector(
-            "div.ui-search-result__content-wrapper div.ui-search-item__group--title a"
-          )
-          .getAttribute("href"),
-        category: document
-          .querySelectorAll("ol.andes-breadcrumb li")
-          [
-            document.querySelectorAll("ol.andes-breadcrumb li").length - 1
-          ].querySelector("a span").textContent,
-      };
-
-      return productInfos;
-    });
-
-    if (productCards.length) {
-      return productCards;
-    } else {
-      const productCardsGrid = [
-        ...document.querySelectorAll("ol.ui-search-layout--grid"),
-      ].map((ol) =>
+  const productCards: IProduct[] = [
+    ...document.querySelectorAll(
+      "ol.ui-search-layout ui-search-layout--stack li.ui-search-layout__item div.ui-search-result__wrapper div.ui-search-result"
+    ),
+  ].map((productCard) => {
+    const productInfos = {
+      description: productCard.querySelector(
+        "div.ui-search-result__content-wrapper div.ui-search-item__group--title a.shops__items-group-details h2.ui-search-item__title"
+      ).textContent,
+      imageUrl: productCard
+        .querySelector(
+          "div.ui-search-result__image a div.slick-track div.slick-active img.ui-search-result-image__element"
+        )
+        .getAttribute("src")
+        .toString(),
+      price: productCard.querySelector(
+        "div.ui-search-result__content-wrapper div.ui-search-result__content-columns div div div div div span span.price-tag-amount"
+      ).textContent,
+      website: productCard
+        .querySelector(
+          "div.ui-search-result__content-wrapper div.ui-search-item__group--title a"
+        )
+        .getAttribute("href"),
+      category: document
+        .querySelectorAll("ol.andes-breadcrumb li")
         [
-          ...ol.querySelectorAll(
-            "li.ui-search-layout__item div.ui-search-result__wrapper div.ui-search-result"
-          ),
-        ].map((productCard) => {
-          const productInfos = {
-            description: productCard.querySelector(
-              "div.ui-search-result__content div.ui-search-result__content-wrapper div.ui-search-item__group--title a.shops__items-group-details h2.ui-search-item__title"
-            ).textContent,
-            imageUrl: productCard
-              .querySelector(
-                "div.ui-search-result__image a div div div div div.slick-slide img"
-              )
-              .getAttribute("src"),
-            price: productCard.querySelector(
-              "div.ui-search-result__content div.ui-search-result__content-wrapper div.ui-search-item__group--price div div div span span.price-tag-amount"
-            ).textContent,
-            website: productCard
-              .querySelector(
-                "div.ui-search-result div.ui-search-result__image a"
-              )
-              .getAttribute("href"),
-            category: document
-              .querySelectorAll("ol.andes-breadcrumb li")
-              [
-                document.querySelectorAll("ol.andes-breadcrumb li").length - 1
-              ].querySelector("a span").textContent,
-          };
+          document.querySelectorAll("ol.andes-breadcrumb li").length - 1
+        ].querySelector("a span").textContent,
+    };
 
-          return productInfos;
-        })
-      );
-
-      const result: IProduct[] = [];
-
-      productCardsGrid.forEach((list) => [
-        result.push(...list.map((item) => item)),
-      ]);
-
-      return result;
-    }
+    return productInfos;
   });
 
-  await browser.close();
+  if (productCards.length) {
+    return productCards;
+  } else {
+    const productCardsGrid = [
+      ...document.querySelectorAll("ol.ui-search-layout--grid"),
+    ].map((ol) =>
+      [
+        ...ol.querySelectorAll(
+          "li.ui-search-layout__item div.ui-search-result__wrapper div.ui-search-result"
+        ),
+      ].map((productCard) => {
+        const productInfos = {
+          description: productCard.querySelector(
+            "div.ui-search-result__content div.ui-search-result__content-wrapper div.ui-search-item__group--title a.shops__items-group-details h2.ui-search-item__title"
+          ).textContent,
+          imageUrl: productCard
+            .querySelector(
+              "div.ui-search-result__image a div div div div div.slick-slide img"
+            )
+            .getAttribute("src"),
+          price: productCard.querySelector(
+            "div.ui-search-result__content div.ui-search-result__content-wrapper div.ui-search-item__group--price div div div span span.price-tag-amount"
+          ).textContent,
+          website: productCard
+            .querySelector("div.ui-search-result div.ui-search-result__image a")
+            .getAttribute("href"),
+          category: document
+            .querySelectorAll("ol.andes-breadcrumb li")
+            [
+              document.querySelectorAll("ol.andes-breadcrumb li").length - 1
+            ].querySelector("a span").textContent,
+        };
 
-  return products;
+        return productInfos;
+      })
+    );
+
+    const result: IProduct[] = [];
+
+    productCardsGrid.forEach((list) => [
+      result.push(...list.map((item) => item)),
+    ]);
+
+    return result;
+  }
 };
 
 const scrapingBuscape = async (url: string) => {
-  const browser = await puppeteer.launch({
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-    headless: true,
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    ignoreHTTPSErrors: true,
+  const response = await fetch(url);
+  const html = await response.text();
+
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+
+  const productCards: IProduct[] = [
+    ...document.querySelectorAll("div.SearchCard_ProductCard__1D3ve"),
+  ].map((productCard) => {
+    const productInfos = {
+      description: productCard.querySelector(
+        "a div.SearchCard_ProductCard_Body__2wM_H div.SearchCard_ProductCard_Description__fGXI3 div.SearchCard_ProductCard_NameWrapper__Gv0x_ div h2"
+      ).textContent,
+      imageUrl: productCard
+        .querySelector(
+          "div.SearchCard_ProductCard_Body__2wM_H div.SearchCard_ProductCard_Image__ffKkn span img"
+        )
+        .getAttribute("src"),
+      price: productCard.querySelector(
+        "a div.SearchCard_ProductCard_Body__2wM_H div.SearchCard_ProductCard_Description__fGXI3 div>p.Text_Text__h_AF6"
+      ).textContent,
+      website: `https://www.buscape.com.br${productCard
+        .querySelector("a")
+        .getAttribute("href")}`,
+      category: document.querySelector("h1").textContent,
+    };
+
+    return productInfos;
   });
 
-  const page = await browser.newPage();
-  await page.goto(url);
-
-  const products = await page.evaluate(() => {
-    const productCards: IProduct[] = [
-      ...document.querySelectorAll("div.SearchCard_ProductCard__1D3ve"),
-    ].map((productCard) => {
-      const productInfos = {
-        description: productCard.querySelector(
-          "a div.SearchCard_ProductCard_Body__2wM_H div.SearchCard_ProductCard_Description__fGXI3 div.SearchCard_ProductCard_NameWrapper__Gv0x_ div h2"
-        ).textContent,
-        imageUrl: productCard
-          .querySelector(
-            "div.SearchCard_ProductCard_Body__2wM_H div.SearchCard_ProductCard_Image__ffKkn span img"
-          )
-          .getAttribute("src"),
-        price: productCard.querySelector(
-          "a div.SearchCard_ProductCard_Body__2wM_H div.SearchCard_ProductCard_Description__fGXI3 div>p.Text_Text__h_AF6"
-        ).textContent,
-        website: `https://www.buscape.com.br${productCard
-          .querySelector("a")
-          .getAttribute("href")}`,
-        category: document.querySelector("h1").textContent,
-      };
-
-      return productInfos;
-    });
-
-    return productCards;
-  });
-
-  await browser.close();
-
-  return products;
+  return productCards;
 };
 
 export const getProductsByCategory = async (search: ISearchByCategory) => {
